80df662b53dfa08b928f93a1fbb4d56c10c0c56d
Thanh Cong Luong <112993467+thanhcong2001@users.noreply.github.com>
2023-07-11 11:23:20 +0700

    cong-dev

--- FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
@@ -15,10 +16,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btn = findViewById(R.id.button);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- FinalProject/app/src/main/java/com/example/finalproject/login/LoginPage.java
+++ FinalProject/app/src/main/java/com/example/finalproject/login/LoginPage.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,15 +26,17 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+@SuppressWarnings("DEPRECATION")
 public class LoginPage extends AppCompatActivity {
     EditText username, password;
     Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_login_page);
         username = findViewById(R.id.editTextTextEmailAddress2);
         password = findViewById(R.id.editTextTextPassword);
         btn = findViewById(R.id.button1);
@@ -39,42 +44,44 @@ public class LoginPage extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    login();
+                login();
             }
         });
         create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent SignUp = new Intent(LoginPage.this,SignUp.class);
+                Intent SignUp = new Intent(LoginPage.this, SignUp.class);
                 startActivity(SignUp);
             }
         });
     }
-        public void login(){
-            LoginRequest loginRequest = new LoginRequest();
-            loginRequest.setUser_Account(username.getText().toString());
-            loginRequest.setUser_Password(password.getText().toString());
-            Call<LoginResponse> loginResponseCall = ApiClient.getUserService().userLogin(loginRequest);
-            loginResponseCall.enqueue(new Callback<LoginResponse>() {
-                @Override
-                public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
-                    if(response.isSuccessful()){
-                        Intent intentLogin = new Intent(LoginPage.this,MainPage.class);
-                        startActivity(intentLogin);
-                        LoginResponse loginResponse = response.body();
-                        new Handler().postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                            }
-                        },700);
-                    }else{
-                        Toast.makeText(LoginPage.this,"Wrong Username or password ", Toast.LENGTH_LONG).show();
-                    }
+
+    public void login() {
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setUser_Account(username.getText().toString());
+        loginRequest.setUser_Password(password.getText().toString());
+        Call<LoginResponse> loginResponseCall = ApiClient.getUserService().userLogin(loginRequest);
+        loginResponseCall.enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                if (response.isSuccessful()) {
+                    Intent intentLogin = new Intent(LoginPage.this, MainPage.class);
+                    startActivity(intentLogin);
+                    LoginResponse loginResponse = response.body();
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                        }
+                    }, 700);
+                } else {
+                    Toast.makeText(LoginPage.this, "Wrong Username or password ", Toast.LENGTH_LONG).show();
                 }
-                @Override
-                public void onFailure(Call<LoginResponse> call, Throwable t) {
-                    Toast.makeText(LoginPage.this,"Throwable "+t.getLocalizedMessage(), Toast.LENGTH_LONG).show();
-                }
-            });
-        }
+            }
+
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+                Toast.makeText(LoginPage.this, "Throwable " + t.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
+}
